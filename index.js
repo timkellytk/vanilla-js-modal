4c24895cb4b4ae1750f23e7595d0a269cc958ab5
Tim Kelly <tim.kelly@4mation.com.au>
2021-04-09 10:18:25 +1000

    The modal is working perfectly for more than one instance

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  /* For every modal data component, we want to add an event listener that event listener gets the child item */
+  const allModals = document.querySelectorAll("#modal");
+
+  console.log(allModals);
+
+  allModals.forEach((m) => {
+    const dataId = m.dataset.micromodalId;
+
+    const modal = document.querySelectorAll(
+      `[data-micromodal-id="${dataId}"]#modal`
+    )[0];
+
+    const modalOpen = document.querySelectorAll(
+      `[data-micromodal-id="${dataId}"]#modal-open`
+    )[0];
+
+    const modalClose = document.querySelectorAll(
+      `[data-micromodal-id="${dataId}"]#modal-close`
+    )[0];
+    const modalCloseBg = document.querySelectorAll(
+      `[data-micromodal-id="${dataId}"]#modal-close-bg`
+    )[0];
+
+    modalOpen.addEventListener("click", () => {
+      modal.classList.add("is-open");
+    });
+
+    modalClose.addEventListener("click", () => {
+      modal.classList.remove("is-open");
+    });
+
+    modalCloseBg.addEventListener("click", function (e) {
+      if (e.target === this) {
+        modal.classList.remove("is-open");
+      }
+    });
+  });
+});
